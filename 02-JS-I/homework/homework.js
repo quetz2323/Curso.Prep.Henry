2e3801c2a98fb784e634132e2b2d26b84ea96666
Quetz <95201569+quetz2323@users.noreply.github.com>
2021-12-01 14:55:54 -0600

    first commit1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'hola';
+const nuevaString = 'hola ahí';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '10';git
+const nuevoNum = 10;
 
 // Crea una variable booleana:
 const nuevoBool = 'true';
